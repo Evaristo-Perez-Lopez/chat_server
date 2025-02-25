43413dda1eb912e6d08529e9d3f7283f87a50d9a
Evaristo-Perez-Lopez <perezlopezevaristo07@outlook.com>
2025-02-24 22:00:42 -0500

    set up HTTP server for socket.io and initialize socket routes

--- app/index.js
+++ app/index.js
@@ -1,16 +1,28 @@
 import express from "express";
+import http from "http";
 import { IndexRoutes } from "./routes/routes.js";
+import { IoRoutes } from "./io_routes/io_routes.js";
+
+// CONFIGURATION
 const application = express();
-
 const port = 3000;
-
-const routes = new IndexRoutes();
 
 application.use(express.json());
 application.use(express.urlencoded({ extended: true }));
 
-routes.initRoutes(application);
+// ROUTES
 
+const routes = new IndexRoutes();
+routes.initRoutes(application);
+const io = new IoRoutes();
+
+// SERVER
+const httpServer = http.createServer(application);
+
+httpServer.listen(3001, () => {
+  console.log("Socket server is running on port 3001");
+});
+io.startSocket(httpServer);
 application.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
